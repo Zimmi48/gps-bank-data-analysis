650463bfbc4f3f3ce604049756aaf15efba56fbe
Théo Zimmermann <theo.zimmermann@ens.fr>
2015-01-25 16:24:37 +0100

    now order transactions by date to be consistent with position ordering

--- analyzer.hs
+++ analyzer.hs
@@ -39,13 +39,13 @@ name_amount_date _ acc = acc
 data Transaction = Transaction { name :: String , amount :: Double , trn_date :: UTCTime } deriving (Show, Eq)
 instance Ord Transaction
 	where compare x y =
-		case compare (amount x) (amount y) of
+		case compare (trn_date x) (trn_date y) of
 			EQ ->
-				case compare (trn_date x) (trn_date y) of
+				case compare (amount x) (amount y) of
 					EQ -> compare (name x) (name y)
 					r -> r
 			r -> r
--- transactions are ordered first by amount then by date and finally by name
+-- transactions are ordered first by date then by amount and finally by name
 
 extractTrnDetails :: String -> Maybe Transaction
 extractTrnDetails input =
