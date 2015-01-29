6e84623833fc2e4c19fc1c8d694fee2bc60da2c1
Théo Zimmermann <theo.zimmermann@ens.fr>
2015-01-29 16:47:57 +0100

    adapted sort tests to new datatype

--- analyzer.hs
+++ analyzer.hs
@@ -21,7 +21,7 @@ main = do
 			--putStrLn . show . and . (\dates -> zipWith (<=) dates (drop 1 dates)) $ map trn_date debits
 			-- The answer was no but now transactions are sorted after extraction.
 			-- test if positions are sorted already
-			--putStrLn . show . and . (\dates -> zipWith (<=) dates (drop 1 dates)) $ map pos_date positions
+			--putStrLn . show . and . (\dates -> zipWith (<=) dates (drop 1 dates)) $ map pntTime positions
 			-- The answer is yes.
 			
 			-- show the three biggest spending
