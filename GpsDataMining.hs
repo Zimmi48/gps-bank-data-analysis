1e812a882e5c9a73980b7149b579d368ee14e1d3
Théo Zimmermann <theo.zimmermann@ens.fr>
2015-02-03 20:33:02 +0100

    little progress

--- GpsDataMining.hs
+++ GpsDataMining.hs
@@ -28,7 +28,7 @@ nextShortTime l =
 -- diameter returns the maximal distance between two points of the sublist
 diameter :: Sublist Point -> Double
 diameter l =
-	if sEmpty l then 0 else
+	if sLength l <= 1 then 0 else
 		let hd = sHead l in
 		let tl = sTail l in
 		sFoldr (\pnt tmpMax -> max tmpMax $ distance hd pnt) (diameter tl) tl
@@ -38,5 +38,21 @@ shortDistance = 30 -- depends on the accuracy of geo points
 sTotalDistance :: Sublist Point -> Double
 sTotalDistance l =
 	if sLength l <= 1 then 0 else
-	let tl = sTail l in
-	sTotalDistance tl + distance (sHead l) (sHead tl)
+		let tl = sTail l in
+		sTotalDistance tl + distance (sHead l) (sHead tl)
+
+timeSpan :: Sublist Point -> NominalDiffTime
+timeSpan l =
+	if sLength l <= 1 then 0 else
+		case (pntTime $ sHead l , sLast l >>= pntTime) of
+		(Just begin , Just end) -> end `diffUTCTime` begin
+		_ -> 0
+
+efficientTravelDistance l = sTotalDistance l * 120 / timeSpan
+
+isEvent l = diameter l <= max shortDistance (efficientTravelDistance l)
+
+-- nextEvent returns the next event (merging the successive 5 minutes events)
+-- and the rest of the list
+--nextEvent :: Sublist Point -> (Sublist Point , Sublist Point)
+--nextEvent l = sTakeSublistsWhile nextShortTime isEvent
