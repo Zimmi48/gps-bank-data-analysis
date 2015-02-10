535102e30c71ba9f2d2f7e1cc8f3107f54e17151
Théo Zimmermann <theo.zimmermann@ens.fr>
2015-02-10 13:42:45 +0100

    a new way of computing intersections which does not lead to so large places

--- GpsData.hs
+++ GpsData.hs
@@ -27,9 +27,10 @@ location lat lon = pt lat lon Nothing Nothing
 
 data Place = Place {
 	place_center :: Location,
-	place_diameter :: Double
+	place_diameter :: Double,
+	place_locs :: [Location]
 } deriving (Show, Eq)
-place locs = foldr1 place_merge $ map (flip Place 0) locs
+place locs = foldr1 place_merge $ map (\c -> Place c 0 [c]) locs
 
 data Position = Position {
 	pos_location :: Location,
@@ -47,25 +48,28 @@ sameLocation l1 l2 =
 	pntLat l1 == pntLat l2 &&
 	pntLon l1 == pntLon l2
 
-place_centers_distance = distance `on` place_center
+inside :: Location -> Place -> Bool
+loc `inside` pl  = distance loc (place_center pl) <= place_diameter pl
 
 contains :: Place -> Place -> Bool
-p1 `contains` p2 = 2 * place_centers_distance p1 p2 <= place_diameter p1 - place_diameter p2
+p1 `contains` p2 = all (`inside` p2) (place_locs p1)
 	
 place_intersect :: Place -> Place -> Bool
-place_intersect p1 p2 = 2 * place_centers_distance p1 p2 <= place_diameter p1 + place_diameter p2
+place_intersect p1 p2 =
+	any (`inside` p2) (place_locs p1) ||
+	any (`inside` p1) (place_locs p2)
 
 place_merge :: Place -> Place -> Place
 place_merge p1 p2 =
 	if p1 `contains` p2 then p1
 	else if p2 `contains` p1 then p2
 	else
-		let dist = place_centers_distance p1 p2 in
+		let dist = (distance `on` place_center) p1 p2 in
 		let new_diameter = dist + (place_diameter p1 + place_diameter p2) / 2 in
 		Place
 			(interpolate (place_center p1) (place_center p2) $
 				1/2 + (place_diameter p2 - place_diameter p1) / (4 * dist)
-			) new_diameter
+			) new_diameter $ union (place_locs p1) (place_locs p2)
 
 {- Functions on lists of locations -}
 	
